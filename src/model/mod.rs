c7c0d59c41dddafbb1abdf76f02f98d8d4c712ba
Jeff Bendixsen <jeff_bendixsen@trimble.com>
2023-12-20 21:44:44 -0800

    Adding generics/trait for CRUD

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,10 +1,7 @@
+mod base;
 mod error;
 mod store;
 pub mod vendor;
-
-// use crate::{Error, Result, ctx::Ctx};
-// use serde::{Deserialize, Serialize};
-// use std::sync::{Arc, Mutex};
 
 pub use self::error::{Error, Result};
 
@@ -28,62 +25,4 @@ impl ModelManager {
         &self.db
     }
 }
-
-// Model Types
-
-// #[derive(Clone, Debug, Serialize)]
-// pub struct Vendor {
-//     pub id: u64,
-//     pub cid: u64, // creator
-//     pub name: String,
-// }
-
-// #[derive(Deserialize )]
-// pub struct VendorForCreate {
-//     pub name: String,
-// }
-
-// // Model Controller
-// #[derive(Clone)]
-// pub struct ModelController {
-//     vendor_store: Arc<Mutex<Vec<Option<Vendor>>>>,
-// }
-
-// // Constructor
-// impl ModelController {
-//     pub async fn new() -> Result<Self> {
-//         Ok(Self{
-//             vendor_store: Arc::default(),
-//         })
-//         // This is what they AI gave me
-//         // let vendor_store = Arc::new(Mutex::new(Vec::new()));
-//         // Ok(Self { vendor_store })
-//     }
-// }
-
-// CRUD
-// impl ModelController {
-//     pub async fn create_vendor(&self, ctx: Ctx, vendor_fc: VendorForCreate) -> Result<Vendor> {
-//         let mut vendor_store = self.vendor_store.lock().unwrap();
-//         let vendor = Vendor {
-//             id: vendor_store.len() as u64,
-//             cid: ctx.user_id(),
-//             name: vendor_fc.name,
-//         };
-//         vendor_store.push(Some(vendor.clone()));
-//         Ok(vendor)
-//     }
-
-//     pub async fn list_vendors(&self, _ctx: Ctx) -> Result<Vec<Vendor>> {
-//         let vendor_store = self.vendor_store.lock().unwrap();
-//         let vendors = vendor_store.iter().filter_map(|v| v.clone()).collect();
-//         Ok(vendors)
-//     }
-
-//     pub async fn delete_vendor(&self, _ctx: Ctx, id: u64) -> Result<(Vendor)> {
-//         let mut vendor_store = self.vendor_store.lock().unwrap();
-//         let vendor = vendor_store.get_mut(id as usize).and_then(|v| v.take());
-//         vendor.ok_or(Error::VendorDeleteFailIdNotFound { id })
-//     }
-// }
 
--- src/model/vendor.rs
+++ src/model/vendor.rs
@@ -1,24 +1,26 @@
 use crate::ctx::Ctx;
 use crate::model::ModelManager;
 use crate::model::{Error, Result};
+use crate::model::base::{self, DbBmc};
 use serde::{Deserialize, Serialize};
+use sqlb::Fields;
 use sqlx::FromRow;
 
 // Vendor Types
 
-#[derive(Clone, Debug, FromRow, Serialize)]
+#[derive(Clone, Debug, Fields, FromRow, Serialize)]
 pub struct Vendor {
     pub id: i64,
     // pub cid: i64, // creator
     pub name: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Fields, Deserialize)]
 pub struct VendorForCreate {
     pub name: String,
 }
 
-#[derive(Deserialize)]
+#[derive(Fields, Deserialize)]
 pub struct VendorForUpdate {
     pub name: Option<String>,
 }
@@ -26,62 +28,40 @@ pub struct VendorForUpdate {
 // Vendor BMC
 pub struct VendorBmc;
 
+impl DbBmc for VendorBmc {
+    const TABLE: &'static str = "vendors";
+}
+
 impl VendorBmc {
     // Create Vendor
     pub async fn create(
-        _ctx: &Ctx,
+        ctx: &Ctx,
         mm: &ModelManager,
         vendor_c: VendorForCreate
     ) -> Result<i64> {
-        let db = mm.db();
-        let (id,) = sqlx::query_as::<_, (i64,)>(
-            "INSERT INTO vendors (name) VALUES ($1) RETURNING id"
-        )
-        .bind(vendor_c.name)
-        .fetch_one(db)
-        .await?;
-
-        Ok(id)
+        base::create::<Self, _>(ctx, mm, vendor_c).await  
     }
 
     // Get Vendor
-    pub async fn get(_ctx: &Ctx, mm: &ModelManager, id: i64) -> Result<Vendor> {
-        let db = mm.db();
-
-        let vendor: Vendor = sqlx::query_as("SELECT * FROM vendors WHERE id = $1")
-            .bind(id)
-            .fetch_optional(db)
-            .await?
-            .ok_or(Error::EntityNotFound { entity: "vendors", id })?;
-
-        Ok(vendor)
+    pub async fn get(ctx: &Ctx, mm: &ModelManager, id: i64) -> Result<Vendor> {
+        base::get::<Self, _>(ctx, mm, id).await
     }
 
-    pub async fn list(_ctx: &Ctx, mm: &ModelManager) -> Result<Vec<Vendor>> {
-        let db = mm.db();
-
-        let vendors: Vec<Vendor> = sqlx::query_as("SELECT * FROM vendors ORDER BY name")
-            .fetch_all(db)
-            .await?;
-
-        Ok(vendors)
+    pub async fn list(ctx: &Ctx, mm: &ModelManager) -> Result<Vec<Vendor>> {
+        base::list::<Self, _>(ctx, mm).await
     }
 
-    // TODO: Update
+    pub async fn update(
+        ctx: &Ctx,
+        mm: &ModelManager,
+        id: i64,
+        vendor_u: VendorForUpdate
+    ) -> Result<()> {
+        base::update::<Self, _>(ctx, mm, id, vendor_u).await
+    }
 
-    pub async fn delete(_ctx: &Ctx, mm: &ModelManager, id: i64) -> Result<()> {
-        let db = mm.db();
-
-        let count = sqlx::query("DELETE FROM vendors WHERE id = $1")
-            .bind(id)
-            .execute(db)
-            .await?
-            .rows_affected();
-
-        if count == 0 {
-            return Err(Error::EntityNotFound { entity: "vendors", id });
-        }
-        Ok(())
+    pub async fn delete(ctx: &Ctx, mm: &ModelManager, id: i64) -> Result<()> {
+        base::delete::<Self>(ctx, mm, id).await
     }
 
 }
@@ -91,111 +71,140 @@ impl VendorBmc {
 #[cfg(test)]
 mod tests {
     #![allow(unused)]
-    use crate::_dev_utils;
-
     use super::*;
+    use crate::_dev_utils;
     use anyhow::Result;
     use serial_test::serial;
 
-    #[serial]
-    #[tokio::test]
-    async fn test_create_ok() -> Result<()> {
-        let mm = _dev_utils::init_test().await;
-        let ctx = Ctx::root_ctx();
-        let fx_name = "test_create_ok title";
+    // #[serial]
+    // #[tokio::test]
+    // async fn test_create_ok() -> Result<()> {
+    //     let mm = _dev_utils::init_test().await;
+    //     let ctx = Ctx::root_ctx();
+    //     let fx_name = "test_create_ok title";
 
-        // -- Exec 
-        let vendor_c = VendorForCreate {
-            name: fx_name.to_string(),
-        };
-        let id = VendorBmc::create(&ctx, &mm, vendor_c).await?;
+    //     // -- Exec 
+    //     let vendor_c = VendorForCreate {
+    //         name: fx_name.to_string(),
+    //     };
+    //     let id = VendorBmc::create(&ctx, &mm, vendor_c).await?;
 
-        // -- Check
-        let vendor = VendorBmc::get(&ctx, &mm, id).await?;
-        assert_eq!(vendor.name, fx_name);
+    //     // -- Check
+    //     let vendor = VendorBmc::get(&ctx, &mm, id).await?;
+    //     assert_eq!(vendor.name, fx_name);
 
-        // -- Clean
-        VendorBmc::delete(&ctx, &mm, id).await?;
+    //     // -- Clean
+    //     VendorBmc::delete(&ctx, &mm, id).await?;
         
-        Ok(())
-    }
+    //     Ok(())
+    // }
+
+    // #[serial]
+    // #[tokio::test]
+    // async fn test_get_err_not_found() -> Result<()> {
+    //     let mm = _dev_utils::init_test().await;
+    //     let ctx = Ctx::root_ctx();
+    //     let id = 100;
+
+    //     // -- Exec 
+    //     let res = VendorBmc::get(&ctx, &mm, id).await;
+
+    //     // -- Check
+    //     assert!(
+    //         matches!(
+    //             res,
+    //             Err(Error::EntityNotFound { 
+    //                 entity: "vendors", 
+    //                 id: 100 
+    //             })
+    //         ),
+    //         "EntityNotFound not matching"
+    //     );
+    //     Ok(())
+    // }
+    
+    // #[serial]
+    // #[tokio::test]
+    // async fn test_list_ok() -> Result<()> {
+    //     // -- Set up fixtures
+    //     let mm = _dev_utils::init_test().await;
+    //     let ctx = Ctx::root_ctx();
+    //     let vendor_names = &["test_list_ok 01", "test_list_ok 02"];
+    //     _dev_utils::seed_vendors(&ctx, &mm, vendor_names).await?;
+        
+    //     // -- Exec
+    //     let vendors = VendorBmc::list(&ctx, &mm).await?;
+        
+    //     // -- Check
+    //     let vendors: Vec<Vendor> = vendors
+    //         .into_iter()
+    //         .filter(|v| v.name.contains("test_list_ok"))
+    //         .collect();
+    //     assert_eq!(vendors.len(), 2, "Number of seeded vendors");
+    
+    //     // -- Clean
+    //     for vendor in vendors.iter() {
+    //         VendorBmc::delete(&ctx, &mm, vendor.id).await?;
+            
+    //     }
+    //     Ok(())
+    // }
 
     #[serial]
     #[tokio::test]
-    async fn test_get_err_not_found() -> Result<()> {
-        let mm = _dev_utils::init_test().await;
-        let ctx = Ctx::root_ctx();
-        let id = 100;
-
-        // -- Exec 
-        let res = VendorBmc::get(&ctx, &mm, id).await;
-
-        // -- Check
-        assert!(
-            matches!(
-                res,
-                Err(Error::EntityNotFound { 
-                    entity: "vendors", 
-                    id: 100 
-                })
-            ),
-            "EntityNotFound not matching"
-        );
-
-        Ok(())
-    }
-
-    #[serial]
-    #[tokio::test]
-    async fn test_delete_err_not_found() -> Result<()> {
+    async fn test_update_ok() -> Result<()> {
         // -- Set up fixtures
         let mm = _dev_utils::init_test().await;
         let ctx = Ctx::root_ctx();
-        let id = 100;
+        let fx_name = "test_update_ok vendor name";
+        let fx_name_new = "test_update_ok vendor new name";
+        let fx_vendor = _dev_utils::seed_vendors(&ctx, &mm, &[fx_name])
+            .await?
+            .remove(0);
 
-        // -- Exec 
-        let res = VendorBmc::delete(&ctx, &mm, id).await;
-
-        // -- Check
-        assert!(
-            matches!(
-                res,
-                Err(Error::EntityNotFound { 
-                    entity: "vendors", 
-                    id: 100 
-                })
-            ),
-            "EntityNotFound not matching"
-        );
-
-        Ok(())
-    }
-
-    #[serial]
-    #[tokio::test]
-    async fn test_list_ok() -> Result<()> {
-        // -- Set up fixtures
-        let mm = _dev_utils::init_test().await;
-        let ctx = Ctx::root_ctx();
-        let vendor_names = &["test_list_ok 01", "test_list_ok 02"];
-        _dev_utils::seed_vendors(&ctx, &mm, vendor_names).await?;
-
-        // -- Exec
-        let vendors = VendorBmc::list(&ctx, &mm).await?;
+        VendorBmc::update(
+            &ctx, 
+            &mm, 
+            fx_vendor.id, 
+            VendorForUpdate {
+                name: Some(fx_name_new.to_string()),
+            }
+        ).await?;
 
         // -- Check
-        let vendors: Vec<Vendor> = vendors
-            .into_iter()
-            .filter(|v| v.name.contains("test_list_ok"))
-            .collect();
-        assert_eq!(vendors.len(), 2, "Number of seeded vendors");
+        let vendor = VendorBmc::get(&ctx, &mm, fx_vendor.id).await?;
+        assert_eq!(vendor.name, fx_name_new);
 
         // -- Clean
-        for vendor in vendors.iter() {
-            VendorBmc::delete(&ctx, &mm, vendor.id).await?;
-        }
-        
+        VendorBmc::delete(&ctx, &mm, fx_vendor.id).await?;
+
         Ok(())
     }
+
+    // #[serial]
+    // #[tokio::test]
+    // async fn test_delete_err_not_found() -> Result<()> {
+    //     // -- Set up fixtures
+    //     let mm = _dev_utils::init_test().await;
+    //     let ctx = Ctx::root_ctx();
+    //     let id = 100;
+
+    //     // -- Exec 
+    //     let res = VendorBmc::delete(&ctx, &mm, id).await;
+
+    //     // -- Check
+    //     assert!(
+    //         matches!(
+    //             res,
+    //             Err(Error::EntityNotFound { 
+    //                 entity: "vendors", 
+    //                 id: 100 
+    //             })
+    //         ),
+    //         "EntityNotFound not matching"
+    //     );
+
+    //     Ok(())
+    // }
 
 }
